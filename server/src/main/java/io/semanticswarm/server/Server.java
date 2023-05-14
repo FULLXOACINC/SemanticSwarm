aa6d64e34ee703152b5c9d4436bae0f5de10eaa1
FULLXOACINC <san91130324@gmail.com>
2023-05-15 01:12:57 +0300

    Split modules

--- server/src/main/java/io/semanticswarm/server/Server.java
+++ server/src/main/java/io/semanticswarm/server/Server.java
@@ -2,14 +2,10 @@ package io.semanticswarm.server;
 
 import io.semanticswarm.model.MemberMemory;
 
-import java.util.List;
-
 public class Server {
     public static void main(String[] args) {
         MemberMemory memberMemory = MemberMemory
                 .builder().build();
-
-
 
         System.out.println("Start server");
     }
